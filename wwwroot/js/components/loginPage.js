89dafd1a4283702bb7213138686a673c053698e3
HamedJP <hamedjafarypooya@gmail.com>
2021-07-24 14:15:31 +0430

    enough for today

---
+++ wwwroot/js/components/loginPage.js
@@ -0,0 +1,51 @@
+/*tslint:disabled*/
+import { userService } from "../services/userService.js";
+let rootDiv = document.createElement("div");
+let div = document.createElement("div");
+// div.className = "mb-3";
+rootDiv.className = "loginPage border border-primary modal-dialog rounded-3";
+rootDiv.style = "";
+
+let form = document.createElement("form");
+form.classList = "modal-content loginForm";
+
+let userIputDiv = document.createElement("div");
+let userIput = document.createElement("input");
+userIput.type = "text";
+userIput.placeholder = "نام کاربری";
+userIput.className = "form-control";
+userIputDiv.style = "margin:12px";
+userIputDiv.appendChild(userIput);
+
+let submitDiv = document.createElement("div");
+let submit = document.createElement("button");
+submit.textContent = "ورود";
+submit.classList = "btn btn-primary";
+submitDiv.style = "margin:12px";
+submitDiv.appendChild(submit);
+
+form.appendChild(userIputDiv);
+form.appendChild(submitDiv);
+rootDiv.appendChild(form);
+
+// rootDiv.event
+form.onsubmit = () => {
+  event.preventDefault();
+  if (userIput.value !== "") {
+    userService.uname = userIput.value;
+    loginPage.onLoginUser();
+  } else {
+    userIput.placeholder = "لطفا یک نام کاربری وارد کنید";
+    userIput.style = "color:red";
+  }
+};
+
+export let loginPage = {
+  div() {
+    return rootDiv;
+  },
+  onLoginUser() {},
+  getUserName() {
+    return userIput.value;
+  },
+};
--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,10 +1,17 @@
 import { sidebar } from "./components/sidebar.js";
 import { chatView } from "./components/chatView.js";
 import * as signalR from "./services/signalRService.js";
-import { loginForm } from "./components/loginForm.js";
+import { loginPage } from "./components/loginPage.js";
+import { userService } from "./services/userService.js";
+
 let app = document.getElementById("App");
 
-app.appendChild(loginForm);
+app.appendChild(loginPage.div());
+
+loginPage.onLoginUser = () => {
+  console.log(`in index page: ${userService.uname}`);
+};
+
 // app.appendChild(sidebar);
 // app.appendChild(chatView);
 
