094df6263b4c94e6e49718211650edddb8991f1d
HamedJP <63074637+HamedJP@users.noreply.github.com>
2021-09-13 01:08:37 +0430

    Update sidebar.js
    show a message if there is only one user

--- wwwroot/js/components/sidebar.js
+++ wwwroot/js/components/sidebar.js
@@ -17,40 +17,46 @@ let usersList = document.createElement("div");
 usersList.classList =
   "list-group list-group-flush border-bottom scrollarea container";
 
-showUsersInsidebarDiv(-1);
+showUsersInSidebarDiv(-1);
 
-function showUsersInsidebarDiv() {
+function showUsersInSidebarDiv() {
   usersList.innerHTML = "";
-  for (let i = 0; i < userService.allUsers.length; i++) {
-    if (userService.allUsers[i].id !== userService.currentUser.id) {
-      let userDiv = document.createElement("div");
-      userDiv.id = `${userService.allUsers[i].id}`;
+  if (1<userService.allUsers.length) {
+    for (let i = 0; i < userService.allUsers.length; i++) {
+      if (userService.allUsers[i].id !== userService.currentUser.id) {
+        let userDiv = document.createElement("div");
+        userDiv.id = `${userService.allUsers[i].id}`;
 
-      let p = document.createElement("p");
-      p.textContent = `${userService.allUsers[i].name}`;
-      p.classList = "col";
-      userDiv.appendChild(p);
-      // userDiv.appendChild(videoCallButton);
-      if (userDiv.id === selectedUserId) {
-        userDiv.classList =
-          "list-group-item list-group-item-action py-3 active lh-tight userContainer";
-      } else {
-        userDiv.classList =
-          "list-group-item list-group-item-action py-3 lh-tight userContainer";
+        let p = document.createElement("p");
+        p.textContent = `${userService.allUsers[i].name}`;
+        p.classList = "col";
+        userDiv.appendChild(p);
+        // userDiv.appendChild(videoCallButton);
+        if (userDiv.id === selectedUserId) {
+          userDiv.classList =
+            "list-group-item list-group-item-action py-3 active lh-tight userContainer";
+        } else {
+          userDiv.classList =
+            "list-group-item list-group-item-action py-3 lh-tight userContainer";
+        }
+
+        userDiv.onclick = () => {
+          selectedUserId = userService.selectUserToCall(userDiv.id);
+
+          showUsersInSidebarDiv();
+        };
+        usersList.appendChild(userDiv);
       }
-
-      userDiv.onclick = () => {
-        selectedUserId = userService.selectUserToCall(userDiv.id);
-
-        showUsersInsidebarDiv();
-      };
-      usersList.appendChild(userDiv);
     }
+  } else {
+    let p=document.createElement("p")
+    p.textContent="You are the only one who connected to this server. At least two users must be connected to be able to communicate through WenRTC."
+    usersList.appendChild(p);
   }
 }
 
 userService.onUserListChanged = () => {
-  showUsersInsidebarDiv();
+  showUsersInSidebarDiv();
 };
 
 sidebarDiv.appendChild(title);
